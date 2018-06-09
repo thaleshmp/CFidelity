dfced48b4eb161dfb738ede1894fec8665483078
thaleshmp <thalespilao@hotmail.com>
2018-06-09 13:17:06 -0300

    adicionando controllers + repository

---
+++ CFidelity.API/Controllers/RootController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net;
+using System.Collections.Generic;
+using System;
+using Microsoft.Extensions.Logging;
+using System.IO;
+
+namespace CFidelity.API.Controllers
+{
+    [Route("v1/root")]
+    public class RootController : Controller
+    {
+        public RootController()
+        {
+        }
+
+        [HttpPost]
+        [Route("")]
+        public IActionResult UploadFile()
+        {
+            try
+            {
+                var file = Request.Form.Files["teste"];
+                List<string> listA = new List<string>();
+                using (var reader = new StreamReader(file.OpenReadStream()))
+                {
+                    while (!reader.EndOfStream)
+                    {
+                        var line = reader.ReadLine();
+                        var values = line.Split(';');
+                        listA.Add(line);
+                    }
+                }
+                
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return StatusCode((int)HttpStatusCode.InternalServerError, ex);
+            }
+        }
+
+        [HttpGet]
+        [Route("")]
+        public IActionResult Get()
+        {
+            return Ok();
+        }
+    }
+}
---
+++ CFidelity.API/Repository/OfertaRepository.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net;
+using System.Collections.Generic;
+using System;
+using Microsoft.Extensions.Logging;
+using System.IO;
+
+namespace CFidelity.API.Controllers
+{
+    public class ValuesController : Controller
+    {
+        public ValuesController()
+        {
+        }
+
+        [HttpGet]
+        public IActionResult GetWithRestriction()
+        {
+            try
+            {
+                //var file = Request.Form.Files[""];
+                //var reader = new StreamReader(file.OpenReadStream());
+                //var csv = new CsvReader(reader);
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return StatusCode((int)HttpStatusCode.InternalServerError, ex);
+            }
+        }
+    }
+}
